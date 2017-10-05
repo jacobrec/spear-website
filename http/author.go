200e4a4ab94a2c1c7fef7bb2f9891178bcdc6869
Jacob Reckhard <jacobreckhard@gmail.com>
2017-10-05 17:39:55 -0600

    Added server code for new posts

--- http/author.go
+++ http/author.go
@@ -1,10 +1,18 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/jacobrec/spearserver/blog"
+    "github.com/jacobrec/spearserver/sql"
+)
 
 func deletePost(c *gin.Context) {
 
 }
 func putPost(c *gin.Context) {
-
+	var post blog.Post
+	c.BindJSON(&post)
+    sql.AddPost(post)
+    fmt.Println(post)
 }
--- writer/main.js
+++ writer/main.js
@@ -4,18 +4,26 @@ var url = "http://localhost:8049"
 function post() {
     let tagset = document.getElementById('tags').value.trim().split(" ");
     let obj = {
-        html: editor.getData(),
         title: document.getElementById('title').value,
         author: document.getElementById('author').value,
+        post: editor.getData(),
+        timestamp: 0,
         tags: tagset
     };
 
     var xhr = new XMLHttpRequest();
     xhr.open('PUT', url + '/blog/', true);
     xhr.setRequestHeader("Content-type", "application/json");
-    xhr.send(obj);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            alert("It Worked!")
+        }
+    };
 
     console.log(obj);
+    xhr.send(JSON.stringify(obj));
+
+
 }
 
 function setup() {
