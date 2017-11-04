4d9c62e0c34f122234a88512dfa456f8c0d5ac30
Jacob Reckhard <jacobreckhard@gmail.com>
2017-11-04 15:03:02 -0600

    did style for main page

--- src/app/site/mainpage/main.component.ts
+++ src/app/site/mainpage/main.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css'],
 })
 export class MainPageComponent {
-  fullname = 'Space Exploration Alberta Project'
+  fullname = 'Space Exploration Alberta Robotics'
 }
