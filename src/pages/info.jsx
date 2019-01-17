5c17744b50164c5893c1d765b27726ecf11f9293
Jacob Reckhard <jacobreckhard@gmail.com>
2019-01-16 20:16:43 -0700

    made info page cleaner

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -3,15 +3,19 @@ import SpearTitle from "../components/SpearTitle"
 
 export default class SpearInfoPage extends Component {
     render(){
+        const vlogs = ["lDlzEwEM7iU", "wTmVX3USq54", "Qt7kQpG_Ohk"]
         return (
             <div>
                 <SpearTitle title="Info" img={require('../img/logos/supporters.svg')}/>
 
                 <div className="ver cent">
                     <h2>Competition Video Log</h2>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/lDlzEwEM7iU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/wTmVX3USq54" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Qt7kQpG_Ohk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    { vlogs.map((v, i) => (
+                        <iframe title={`vlog${i+1}`} key={v} width="560" height="315"
+                            src={`https://www.youtube.com/embed/${v}`}
+                            frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
+                    ))}
                 </div>
             </div>
         )
