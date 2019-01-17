e8fa4a3b383a32a7a757a81a9a1062e3a57b336c
Jacob Reckhard <jacobreckhard@gmail.com>
2019-01-16 19:58:46 -0700

    got stupid sponsor images to scale properly

--- src/pages/sponsor.jsx
+++ src/pages/sponsor.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import SpearTitle from "../components/SpearTitle"
+
 import './sponsor.css'
 
 function Sponsor(props){
   return (
     <div className={"hor" + (props.isLeft ? "" : " reverse")}>
       <div className="sponsor-image-holder">
-          <img  alt={props.name + " logo"} src={require('../img/sponsors/' + props.imgPath)}/>
+        {console.log(require('../img/sponsors/' + props.imgPath))}
+        <img alt={props.name + " logo"} src={require('../img/sponsors/' + props.imgPath)}/>
       </div>
       <div className="sponsor-text-holder">
         <p>{props.description}</p>
@@ -20,21 +22,18 @@ export default class SpearSponsorPage extends Component {
   render(){
     return (
       <div className="ver">
-        <SpearTitle title="Sponsors"/>
+        <SpearTitle title="Sponsors" img={require('../img/logos/supporters.svg')}/>
         <div className="sponsor-intro">
-        <p>
-          Without the support of our sponsors, our mission would not be
-          possible. Our sponsors have allowed our team to incredible things in
-          the in the past year and we hope to do more incredible things in the
-          future. So, thank you for your support.
-        </p>
-      </div>
-        <div className="ver spread">
-          <Sponsor name="shell" imgPath="shell.png" isLeft={true} description=
-            "The Shell Canada Enhanced Learning Fund (SELF) provides funding
-            for University of Alberta students to participate in experiential
-            learning opportunities that have a focus on sustainable energy, the
-            environment and the economy. https://www.shell.ca/"/>
+          <p>
+            Without the support of our sponsors, our mission would not be
+            possible. Our sponsors have allowed our team to incredible things in
+            the in the past year and we hope to do more incredible things in the
+            future. So, thank you for your support.
+          </p>
+        </div>
+        <div>
+          <Sponsor name="UofA" imgPath="uofa.png" isLeft={true} description=
+            "UAlberta is a Top 5 Canadian university and one of the Top 100 in the world, home to more than 500 graduate programs, 200 undergraduate programs and 450 active student groups. https://www.ualberta.ca/"/>
           <Sponsor name="shell" imgPath="shell.png" isLeft={false} description=
             "The Shell Canada Enhanced Learning Fund (SELF) provides funding
             for University of Alberta students to participate in experiential
