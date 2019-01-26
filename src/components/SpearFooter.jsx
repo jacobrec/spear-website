c88acf649337433beced6556ab29b74a0cdc3ae7
Jacob Reckhard <jacobreckhard@gmail.com>
2019-01-25 21:05:43 -0700

    footer bar, closes #29

--- src/components/SpearFooter.jsx
+++ src/components/SpearFooter.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 export default class SpearFooter extends Component {
   render() {
     return (
-      <div className="spear-footer ver drift">
-        <h2>CONTACT US</h2>
-        <a href="mailto:spear@ualberta.ca">spear@ualberta.ca</a>
-        <a href="https://www.instagram.com/spaceualberta/">instagram: @spaceualberta</a>
-        <a href="http://twitter.com/spaceualberta">twitter: @spaceualberta</a>
-        <a href="http://facebook.com/SpaceUAlberta">facebook: facebook.com/SpaceUAlberta</a>
-      </div>
+        <div className="spear-footer ver drift" style={{alignItems: "center"}}>
+          <h2 style={{margin: "1.2em"}}>CONTACT US</h2>
+          <a href="mailto:spear@ualberta.ca">spearua@ualberta.ca</a>
+          <a href="https://www.instagram.com/spaceualberta/">instagram: @spaceualberta</a>
+          <a href="http://twitter.com/spaceualberta">twitter: @spaceualberta</a>
+          <a style={{marginBottom: "1.5em"}} href="http://facebook.com/SpaceUAlberta">facebook: facebook.com/SpaceUAlberta</a>
+        </div>
     )
   }
 }
