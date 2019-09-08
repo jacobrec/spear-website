5e52abfa7a183a471d1cfe2608df9615194b2ef2
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-08 00:14:54 -0600

    simplified spearfooter

--- src/components/SpearFooter.jsx
+++ src/components/SpearFooter.jsx
@@ -1,15 +1,18 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export default class SpearFooter extends Component {
-  render() {
-    return (
-        <div className="spear-footer ver drift" style={{alignItems: "center"}}>
-          <h2 style={{margin: "1.2em"}}>CONTACT US</h2>
-          <a href="mailto:spear@ualberta.ca">spearua@ualberta.ca</a>
-          <a href="https://www.instagram.com/spaceualberta/">instagram: @spaceualberta</a>
-          <a href="http://twitter.com/spaceualberta">twitter: @spaceualberta</a>
-          <a style={{marginBottom: "1.5em"}} href="http://facebook.com/SpaceUAlberta">facebook: facebook.com/SpaceUAlberta</a>
-        </div>
-    )
-  }
-}
+export default () => (
+  <div className="spear-footer ver drift" style={{ alignItems: "center" }}>
+    <h2 style={{ margin: "1.2em" }}>CONTACT US</h2>
+    <a href="mailto:spear@ualberta.ca">spearua@ualberta.ca</a>
+    <a href="https://www.instagram.com/spaceualberta/">
+      instagram: @spaceualberta
+    </a>
+    <a href="http://twitter.com/spaceualberta">twitter: @spaceualberta</a>
+    <a
+      style={{ marginBottom: "1.5em" }}
+      href="http://facebook.com/SpaceUAlberta"
+    >
+      facebook: facebook.com/SpaceUAlberta
+    </a>
+  </div>
+);
