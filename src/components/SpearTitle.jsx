0cb7fe6dbaa1dc93f2a21914bd359d54f7ab9fbe
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-08 00:17:46 -0600

    simplified speartitle

--- src/components/SpearTitle.jsx
+++ src/components/SpearTitle.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
+import React from "react";
 
 const styleDiv = {
-  width: '30vw',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-around',
+  width: "30vw",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-around"
 };
 
 const styleDivOuter = {
-  width: '100vw',
-  backgroundColor: 'var(--spear-green)',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-around',
-  marginBottom: '20px'
+  width: "100vw",
+  backgroundColor: "var(--spear-green)",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-around",
+  marginBottom: "20px"
 };
 
 const styleHeader = {
-  textTransform: 'uppercase',
+  textTransform: "uppercase",
   fontSize: "3em"
 };
 
@@ -25,16 +25,17 @@ const styleImg = {
   height: "3em"
 };
 
-export default class SpearTitle extends Component {
-  render() {
-    return (
-      <div style={styleDivOuter}>
-        <div style={styleDiv}>
-          {this.props.img && <img alt="A svg logo for decoration" style={styleImg} src={this.props.img}/> }
-          <h1 style={styleHeader}>{this.props.title}</h1>
-        </div>
-      </div>
-    )
-  }
-}
-
+export default () => (
+  <div style={styleDivOuter}>
+    <div style={styleDiv}>
+      {this.props.img && (
+        <img
+          alt="A svg logo for decoration"
+          style={styleImg}
+          src={this.props.img}
+        />
+      )}
+      <h1 style={styleHeader}>{this.props.title}</h1>
+    </div>
+  </div>
+);
