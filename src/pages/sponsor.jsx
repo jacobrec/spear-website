b9c921e82e1f1eb4ad20c48085cea617bb0250dc
Christiana Garros <christianawgarros@gmail.com>
2020-08-31 17:38:08 -0600

    updated the sponsor page

--- src/pages/sponsor.jsx
+++ src/pages/sponsor.jsx
@@ -24,8 +24,11 @@ function Sponsor(props){
 
 const uofa = { img: "uofa.png", description: "UAlberta is a Top 5 Canadian university and one of the Top 100 in the world, home to more than 500 graduate programs, 200 undergraduate programs and 450 active student groups. https://www.ualberta.ca/" }
 const shell = { img: "shell.png", description: "The Shell Canada Enhanced Learning Fund (SELF) provides funding for University of Alberta students to participate in experiential learning opportunities that have a focus on sustainable energy, the environment and the economy. https://www.shell.ca/" }
+const protocase = { img: "protocase.png", description: "Protocase is a company that manufactures custom sheet metal and CNC machined electronic encolsures for mounting hardware and electronics. Customers provide a design, and they machine it. https://www.protocase.com/"}
+const Digikey = { img: "digi-key.png", description: "Digi-key is a large electronic component distributor in North America and a broad-line distributor of board level components. https://www.digikey.ca/" }
+const Solidworks = { img: "solidworks.png", description: "Solidworks is a solid modeling computer-aided design (CAD) and computer-aided engineering (CAE) software used by millions of engineers and designers worldwide. https://www.solidworks.com/" }
 
-const sponsors = [uofa, shell, shell, shell]
+const sponsors = [uofa, shell, protocase, Digikey, Solidworks]
 export default class SpearSponsorPage extends Component {
   render() {
     return (
